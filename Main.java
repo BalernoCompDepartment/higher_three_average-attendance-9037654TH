4f6238c8cda010e95e0433285531280513ff8639
9037654TH <77011500+9037654TH@users.noreply.github.com>
2021-01-20 11:00:48 +0000

    program complete

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int total = 0;
+    for (int index = 0; index < 8; index++){
+      int attendance = Keyboard.getInt(" ");
+      total = total + attendance;
+    }
+
+    double average = (double) total / 8;
+
+    int roundAverage = (int) Math.round(average);
+
+    System.out.println("Hello world " + roundAverage + " ");
   }
 }
